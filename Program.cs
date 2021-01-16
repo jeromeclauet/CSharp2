77a3f814255a703fd2ffa5be7d953ca9658feda2
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-16 11:05:29 +0100

    Exercice 1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rainy = true;
+            if (rainy == true)
+            {
+                Console.WriteLine("Bonjour, il pleut aujourd’hui, vous devriez prendre votre parapluie.");
+            }
+            else
+            {
+                Console.WriteLine("Bonjour, il ne pleut pas aujourd’hui, bonne journée.");
+            }
+        }
+    }
+}
